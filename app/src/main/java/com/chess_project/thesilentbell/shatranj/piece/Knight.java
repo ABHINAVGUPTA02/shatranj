7d1b2c50aa5fc0e2e764ade2f007ab9c4c5dce33
ABHINAVGUPTA02 <abhinav.gupta.ag47@gmail.com>
2023-06-26 19:39:01 +0530

    Exceptions added for Knight piece

--- app/src/main/java/com/chess_project/thesilentbell/shatranj/piece/Knight.java
+++ app/src/main/java/com/chess_project/thesilentbell/shatranj/piece/Knight.java
@@ -2,10 +2,12 @@ package com.chess_project.thesilentbell.shatranj.piece;
 
 import com.chess_project.thesilentbell.shatranj.Alliance;
 import com.chess_project.thesilentbell.shatranj.board.Board;
+import com.chess_project.thesilentbell.shatranj.board.BoardUtils;
 import com.chess_project.thesilentbell.shatranj.board.Move;
 import com.chess_project.thesilentbell.shatranj.board.Tile;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
@@ -18,22 +20,27 @@ public class Knight extends Piece{
     }
 
     @Override
-    public List<Move> calculateLegalMoves(Board board) {
+    public Collection<Move> calculateLegalMoves(Board board) {
         int candidateDestinationCoordinate;
         final List<Move> LegalMoves = new ArrayList<>();
 
-        for(final int currentCandidate: CANDIDATE_MOVE_COORDINATES){
-            candidateDestinationCoordinate = this.piecePosition + currentCandidate;
+        for (final int currentCandidateOffset : CANDIDATE_MOVE_COORDINATES) {
+            candidateDestinationCoordinate = this.piecePosition + currentCandidateOffset;
 
-            if(true /*isValidCoordinate*/){
-                final Tile candinateDestinationTile =  board.getTile(candidateDestinationCoordinate);
-                if(!candinateDestinationTile.isTileOccupied()){
+            if (BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate)) {
+
+                if(isFirstColumnExclusion(this.piecePosition,currentCandidateOffset) || isSecondColumnExclusion(this.piecePosition,currentCandidateOffset) || isSeventhColumnExclusion(this.piecePosition,currentCandidateOffset) || isEightColumnExclusion(this.piecePosition,currentCandidateOffset)){
+                    continue;
+                }
+
+                final Tile candinateDestinationTile = board.getTile(candidateDestinationCoordinate);
+                if (!candinateDestinationTile.isTileOccupied()) {
                     LegalMoves.add(new Move());
-                }else{
+                } else {
                     final Piece pieceAtDestination = candinateDestinationTile.getPiece();
                     final Alliance pieceAlliance = pieceAtDestination.getPieceAlliance();
 
-                    if(this.pieceAlliance != pieceAlliance){
+                    if (this.pieceAlliance != pieceAlliance) {
                         LegalMoves.add(new Move());
                     }
                 }
@@ -42,4 +49,21 @@ public class Knight extends Piece{
 
         return Collections.unmodifiableList(LegalMoves);
     }
+
+    private static boolean isFirstColumnExclusion(final int currentPosition,final int candidateOffset){
+        return BoardUtils.FIRST_COLUMN[currentPosition] && ((candidateOffset == -17) || (candidateOffset == -10) || (candidateOffset == 6) || (candidateOffset == 15));
+    }
+
+    private static boolean isSecondColumnExclusion(final int currentPosition,final int candidateOffset){
+        return BoardUtils.SECOND_COLUMN[currentPosition] && ((candidateOffset == -10) || (candidateOffset == 6));
+    }
+
+    private static boolean isSeventhColumnExclusion(final int currentPosition,final int candidateOffset){
+        return BoardUtils.SEVENTH_COLUMN[currentPosition] && ((candidateOffset == 10) || (candidateOffset == -6));
+    }
+
+    private static boolean isEightColumnExclusion(final int currentPosition,final int candidateOffset){
+        return BoardUtils.EIGHTH_COLUMN[currentPosition] && ((candidateOffset == -15) || (candidateOffset == -6) || (candidateOffset == 10) || (candidateOffset == 17));
+    }
+
 }
--- app/src/main/java/com/chess_project/thesilentbell/shatranj/piece/Piece.java
+++ app/src/main/java/com/chess_project/thesilentbell/shatranj/piece/Piece.java
@@ -4,7 +4,7 @@ import com.chess_project.thesilentbell.shatranj.Alliance;
 import com.chess_project.thesilentbell.shatranj.board.Board;
 import com.chess_project.thesilentbell.shatranj.board.Move;
 
-import java.util.List;
+import java.util.Collection;
 
 public abstract class Piece {
     protected final int piecePosition;
@@ -19,5 +19,5 @@ public abstract class Piece {
         return this.pieceAlliance;
     }
 
-    public abstract List<Move> calculateLegalMoves(final Board board);
+    public abstract Collection<Move> calculateLegalMoves(final Board board);
 }
